4ea7c972a1ed7994fb110609db6189e3eb14d9a9
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-03-14 15:23:01 +0000

    should remember to include the new files.

---
+++ inc/communityPartners.php
@@ -0,0 +1,11 @@
+<!--
+<section id="community">
+        <div class="content">
+                <h2>Thanks to our Community Partners</h2>
+                <ul>
+                        <li><a href="http://www.londonwebmeetup.org"><img src="./images/londonweb.png" height="85" width="99" alt="London Web."/></a></li>
+                        <li><a href="http://www.phplondon.org"><img src="./images/phplondon.png" height="85" width="142" alt="PHP London."/></a></li>
+                </ul>
+        </div>
+</section>
+--> 
---
+++ inc/header.php
@@ -0,0 +1,8 @@
+<header>
+    <h1 class="summary">
+            <span>State Of The Browser</span>
+    </h1>
+    <h2 class="date">
+            <span>Ravensbourne, 9 to 5pm Saturday, April 28th 2012</span>
+    </h2>
+</header>

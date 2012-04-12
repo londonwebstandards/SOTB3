eda729b60aaaeea2471f097b6b0f97ab190cd522
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-12 17:37:05 +0100

    Twitter button.

--- inc/pageopen.php
+++ inc/pageopen.php
@@ -26,6 +26,7 @@
                 <a href="index.php">State of the browser 2</a>
             </div>
             <div class="four columns social">
-                Tweet this
+                <a href="https://twitter.com/share" class="twitter-share-button" data-size="large">Tweet</a>
+				<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0];if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src="//platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
             </div>
         </header>

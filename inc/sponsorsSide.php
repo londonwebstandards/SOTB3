28b85045dfce26b4647b0257850b4787ef84d8a8
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-12 14:52:16 +0100

    Sponsor logos

--- inc/sponsorsSide.php
+++ inc/sponsorsSide.php
@@ -1,12 +1,12 @@
 <div id="sponsors" class="four columns offset-by-one">
     <h2>Sponsors</h2>
     <ul>
-        <li><a href="#"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
-        <li><a href="#"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>
-        <li><a href="#"><img src="img/assanka.png" alt="Assanka" height="63" width="160"></a></li>
-        <li><a href="#"><img src="img/potato.png" alt="Potato" height="182" width="160"></a></li>
-        <li><a href="#"><img src="img/twilio.png" alt="Twilio, Cloud Communications" height="66" width="160"></a></li>
-        <li><a href="#"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a></li>
-        <li><a href="#"><img src="img/creative-js.png" alt="E-Noise" height="56" width="160"></a></li>
+        <li><a href="http://squiz.com"><img src="/img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
+        <li><a href="http://moo.com"><img src="/img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>
+        <li><a href="http://assanka.com"><img src="/img/assanka.png" alt="Assanka" height="63" width="160"></a></li>
+        <li><a href="http://potato.com"><img src="/img/potato.png" alt="Potato" height="182" width="160"></a></li>
+        <li><a href="http://twilio.com"><img src="/img/twilio.png" alt="Twilio, Cloud Communications" height="66" width="160"></a></li>
+        <li><a href="http://enoise.com"><img src="/img/e-noise.png" alt="E-Noise" height="82" width="160"></a></li>
+        <li><a href="http://creativejs.com"><img src="/img/creative-js.png" alt="E-Noise" height="56" width="160"></a></li>
     </ul>
 </div>

216ed5623729367db0b028bf94714ae48714340f
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-12 11:33:22 +0100

    mark up schedule as a basic list.

--- schedule.php
+++ schedule.php
@@ -6,68 +6,49 @@
     
     <div class="eleven columns">
         
-        9:00	Registration
-        9:45	Introduction from Michael Mahemoff
-        10:15	No app is an island
+        <ul>
+		<li>9:00	Registration</li>
+        <li>9:45	Introduction from Michael Mahemoff</li>
+        <li>10:15	No app is an island
         Description: The web is like a continent with roads linking everywhere to everywhere.  Apps are like isolated islands in an uncharted sea.  Learn how Web Intents builds bridges between these apps and to the rest of the web.  "
 
-        Paul Kinlan, Google
-        10:55	Break
-        11:10	The Web As It Should Be
+        Paul Kinlan, Google</li>
+        <li>10:55	Break</li>
+        <li>11:10	The Web As It Should Be
         Martin Beeby(@thebeebs), Internet Explorer
 
         With the advent of CSS3 and the ever-greater maturity of HTML5, it’s an exciting time for the Web. The possibilities for creating truly engaging – even addictive – Web and business applications appear limitless. And, with the release of Internet Explorer 9, Microsoft finally came in from the cold after many years in the world wide wilderness.
 
-        As Internet Explorer moves towards full support of standards-based HTML5 and CSS3, this is a great opportunity to hear from Martin Beeby about how these key technologies will be used to build and deliver the applications of tomorrow, engage end-users like never before, and generate even greater loyalty to Web-sites."
-        11:50	Chris Heillman, Mozilla
-        12:30	Lunch (a varied complementary lunch will be provided)
-        13:30	HTML5: A matter of good form
+        As Internet Explorer moves towards full support of standards-based HTML5 and CSS3, this is a great opportunity to hear from Martin Beeby about how these key technologies will be used to build and deliver the applications of tomorrow, engage end-users like never before, and generate even greater loyalty to Web-sites."</li>
+        <li>11:50	Chris Heillman, Mozilla</li>
+        <li>12:30	Lunch (a varied complementary lunch will be provided)</li>
+        <li>13:30	HTML5: A matter of good form
         Patrick Lauke (@patrick_h_lauke), Opera
 
-        TBC
-        13:25	Break
-        13:40 Breakout session 1 
-        14:50 Breakout session 2
-        Anything marked with * will be repeated across both breakout sessions.	
-        Room 205, 13:40 - Martin Beeby "HTML5 Human Interaction (Kinect, Touch, Ink, Speech etc
+        TBC</li>
+        <li>13:25	Break</li>
+        <li>13:40 Breakout session 1 </li>
+        <li>14:50 Breakout session 2</li>
+        <li>Anything marked with * will be repeated across both breakout sessions.	
+        <ul><li>Room 205, 13:40 - Martin Beeby "HTML5 Human Interaction (Kinect, Touch, Ink, Speech etc
         Laurian Gridinoc (gridinoc), Jetpack Ambassador
-        A walk-through an easy way of creating Firefox Add-ons only with HTML, CSS and JavaScript.
-        Room 205, 14:50 - BlackBerry WebWorks Bootcamp
-        Sanyu Kiruluta (@BlackBerryDev), Research In Motion
-        Room 208 - The Dos and Don'ts on the Mobile Web *
-        Mathew Staikos (@BlackBerryDev); Manager, Browser & Web Platform - Blackberry WebWorks Platform, Research In Motion
-        Room 209 - HTML5 Canvas: The Future of Graphics on the Web *
-        Rob Hawkes (@robhawkes)
-        Room 211 - An introduction to Automated Browser Testing *
-        Simon Stewart (@shs96c), WebDriver Team Lead (Selenium)
-        Room 212 - Performance Optimization for HTML5 Apps *
-        Malte Ubl (@cramforce), Google
-        Room 213 - New CSS3 features plus Opera DragonFly *
-        Chris Mills, Opera
-        15:50	Break
-        16:10	Panel Q&A Discussion Chaired by Jim O'Donnell (@pekingspring)
-        17:00	Drinks at the Pilot Inn
-
-
-        Last year’s breakout sessions
-        13:40 Breakout session 1 
-        14:50 Breakout session 2
-        Anything marked with * will be repeated across both breakout sessions.	
-        Room 205, 13:40 - Mozilla Add-on SDK (née Jetpack)
-        Laurian Gridinoc (gridinoc), Jetpack Ambassador
-        A walk-through an easy way of creating Firefox Add-ons only with HTML, CSS and JavaScript.
-        Room 205, 14:50 - BlackBerry WebWorks Bootcamp
-        Sanyu Kiruluta (@BlackBerryDev), Research In Motion
-        Room 208 - The Dos and Don'ts on the Mobile Web *
-        Mathew Staikos (@BlackBerryDev); Manager, Browser & Web Platform - Blackberry WebWorks Platform, Research In Motion
-        Room 209 - HTML5 Canvas: The Future of Graphics on the Web *
-        Rob Hawkes (@robhawkes)
-        Room 211 - An introduction to Automated Browser Testing *
-        Simon Stewart (@shs96c), WebDriver Team Lead (Selenium)
-        Room 212 - Performance Optimization for HTML5 Apps *
-        Malte Ubl (@cramforce), Google
-        Room 213 - New CSS3 features plus Opera DragonFly *
-        Chris Mills, Opera
+        A walk-through an easy way of creating Firefox Add-ons only with HTML, CSS and JavaScript.</li>
+        <li>Room 205, 14:50 - BlackBerry WebWorks Bootcamp
+        Sanyu Kiruluta (@BlackBerryDev), Research In Motion</li>
+        <li>Room 208 - The Dos and Don'ts on the Mobile Web *
+        Mathew Staikos (@BlackBerryDev); Manager, Browser & Web Platform - Blackberry WebWorks Platform, Research In Motion</li>
+        <li>Room 209 - HTML5 Canvas: The Future of Graphics on the Web *
+        Rob Hawkes (@robhawkes)</li>
+        <li>Room 211 - An introduction to Automated Browser Testing *
+        Simon Stewart (@shs96c), WebDriver Team Lead (Selenium)</li>
+        <li>Room 212 - Performance Optimization for HTML5 Apps *
+        Malte Ubl (@cramforce), Google</li>
+        <li>Room 213 - New CSS3 features plus Opera DragonFly *
+        Chris Mills, Opera</li></ul></li>
+        <li>15:50	Break</li>
+        <li>16:10	Panel Q&A Discussion Chaired by Jim O'Donnell (@pekingspring)</li>
+        <li>17:00	Drinks at the Pilot Inn</li>
+		</ul>
 
         
     </div>

60318fed9e49801a4eb6e9fbe3cbc7b19d67c21c
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-13 10:31:00 +0100

    Fix some link styling

--- sponsors.php
+++ sponsors.php
@@ -15,17 +15,17 @@
 				other innovative new ways of bringing FT content and services to
 				readers wherever they are</p>
 			</li>
-            <li><a href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
-	        <li><a href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>        
+            <li><a class="logo" href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
+	        <li><a class="logo" href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>        
         </ul>
     </div>
 
     <div class="eight columns">
         <ul>
-			<li><a href="http://p.ota.to/"><img src="img/potato.png" alt="Potato" height="182" width="160"></a></li>
-            <li><a href="http://twilio.com"><img src="img/twilio.png" alt="Twilio, Cloud Communications" height="66" width="160"></a></li>
-	        <li><a href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a></li>
-	        <li><a href="http://creativejs.com"><img src="img/creative-js.png" alt="E-Noise" height="56" width="160"></a></li>           
+			<li><a class="logo" href="http://p.ota.to/"><img src="img/potato.png" alt="Potato" height="182" width="160"></a></li>
+            <li><a class="logo" href="http://twilio.com"><img src="img/twilio.png" alt="Twilio, Cloud Communications" height="66" width="160"></a></li>
+	        <li><a class="logo" href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a></li>
+	        <li><a class="logo" href="http://creativejs.com"><img src="img/creative-js.png" alt="E-Noise" height="56" width="160"></a></li>           
         </ul>
     </div>
 </div>

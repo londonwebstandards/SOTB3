fae2d4a03f7e1916843c52e5e35f528a071b3b51
Jason Grant <jason@flexewebs.com>
2012-04-13 12:09:45 +0100

    Added Patrick as a speaker

--- index.php
+++ index.php
@@ -27,13 +27,16 @@
                     <h3>Peter Gasston (<a href="http://twitter.com/stopsatgreen/">@stopsatgreen</a>)</h3>
                 </li>
                 <li>
+                    <h3>Patrick Lauke (<a href="https://twitter.com/patrick_h_lauke">@patrick_h_lauke</a>)</h3>
+                </li>
+                <li>
                     <h3>Paul Kinlan (<a href="http://twitter.com/paul_kinlan/">@paul_kinlan</a>)</h3>
                 </li>
                 <li>
                     <h3>Jason Grant (<a href="http://twitter.com/flexewebs/">@flexewebs</a>)</h3>
                 </li>
                 <li>
-                    <h3>Seb Lee-Delisle (<a href="http://twitter.com/sebly/">@sebly</a>)</h3>
+                    <h3>Seb Lee-Delisle (<a href="http://twitter.com/seb_ly/">@seb_ly</a>)</h3>
                 </li>
                 <li>
                     <h3>Rich Tibbett (<a href="http://twitter.com/richtibbett/">@richtibbett</a>)</h3>

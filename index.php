bbf8d9657b51039a622db9e8e56ddb3e976044f0
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-13 13:57:55 +0100

    Style speaker names.

--- index.php
+++ index.php
@@ -15,31 +15,30 @@
             <h2>Presenters include…</h2>
             <ul>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/1150432577/speakhdr.jpg" alt="@mahemoff"> Michael Mahemoff (<a href="http://twitter.com/mahemoff/">@mahemoff</a>)
-                </li>
+                    <img src="http://a0.twimg.com/profile_images/1150432577/speakhdr.jpg" alt="@mahemoff"> <span class="name fn">Michael Mahemoff</span>             </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/1648506030/denmark.png" alt="@thebeebs"> Martin Beeby (<a href="http://twitter.com/thebeebs/">@thebeebs</a>)
+                    <img src="http://a0.twimg.com/profile_images/1648506030/denmark.png" alt="@thebeebs"> <span class="name fn">Martin Beeby</span>
                 </li>
 				<li>
-                    <img src="http://a0.twimg.com/profile_images/1213141308/redux-yellow.jpg" alt="@patrick_h_lauke"> Patrick Lauke (<a href="http://twitter.com/patrick_h_lauke/">@patrick_h_lauke</a>)
+                    <img src="http://a0.twimg.com/profile_images/1213141308/redux-yellow.jpg" alt="@patrick_h_lauke"> <span class="name fn">Patrick Lauke</span>
                 </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/1666904408/codepo8.png" alt="@codepo8"> Christian Heillman (<a href="http://twitter.com/codepo8/">@codepo8</a>)
+                    <img src="http://a0.twimg.com/profile_images/1666904408/codepo8.png" alt="@codepo8"> <span class="name fn">Christian Heillman</span>
                 </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/391831398/peter_opie.png" alt="@stopsatgreen"> Peter Gasston (<a href="http://twitter.com/stopsatgreen/">@stopsatgreen</a>)
+                    <img src="http://a0.twimg.com/profile_images/391831398/peter_opie.png" alt="@stopsatgreen"> <span class="name fn">Peter Gasston</span>
                 </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/1280643875/me2.jpg" alt="@paul_kinlan"> Paul Kinlan (<a href="http://twitter.com/paul_kinlan/">@paul_kinlan</a>)
+                    <img src="http://a0.twimg.com/profile_images/1280643875/me2.jpg" alt="@paul_kinlan"> <span class="name fn">Paul Kinlan</span> 
                 </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/150326469/n835305082_6420882_6480795.jpg" alt="@flexewebs"> Jason Grant (<a href="http://twitter.com/flexewebs/">@flexewebs</a>)
+                    <img src="http://a0.twimg.com/profile_images/150326469/n835305082_6420882_6480795.jpg" alt="@flexewebs"> <span class="name fn">Jason Grant</span>
                 </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/1232663381/5388246284_df5b90b720_b-1.jpeg" alt="@seb_ly"> Seb Lee-Delisle (<a href="http://twitter.com/seb_ly/">@seb_ly</a>)
+                    <img src="http://a0.twimg.com/profile_images/1232663381/5388246284_df5b90b720_b-1.jpeg" alt="@seb_ly"> <span class="name fn">Seb Lee-Delisle</span>
                 </li>
                 <li>
-                    <img src="http://a0.twimg.com/profile_images/1539099529/me4.jpg" alt="@richtibbet"> Rich Tibbett (<a href="http://twitter.com/richtibbett/">@richtibbett</a>)
+                    <img src="http://a0.twimg.com/profile_images/1539099529/me4.jpg" alt="@richtibbet"> <span class="name fn">Rich Tibbett</span>
                 </li>
             </ul>
         </div>            

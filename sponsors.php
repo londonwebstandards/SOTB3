dd0c82e55476d614af7bae855a1b5d85b226e281
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-13 10:27:22 +0100

    Blurb for FT Labs

--- sponsors.php
+++ sponsors.php
@@ -6,9 +6,17 @@
     <div id="sponsors">
     <div class="eight columns">
         <ul>
+			<li>
+				<a href="http://labs.ft.com"><img src="img/ft-labs.png" alt="Assanka" height="63" width="160"></a>
+				<p>We are one of the world’s leading business news and information
+				organisations, recognised for our authority, integrity and accuracy.
+				FT Labs is an emerging web technologies group within the FT,
+				responsible for producing our HTML5 web app at <a href="http://app.ft.com">app.ft.com</a>, as well as
+				other innovative new ways of bringing FT content and services to
+				readers wherever they are</p>
+			</li>
             <li><a href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
-	        <li><a href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>
-	        <li><a href="http://assanka.com"><img src="img/assanka.png" alt="Assanka" height="63" width="160"></a></li>        
+	        <li><a href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>        
         </ul>
     </div>
 

df25956172a2eac93a025dc28d613ffa1185de27
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-13 13:38:58 +0100

    Home page presenters list.

--- index.php
+++ index.php
@@ -11,35 +11,35 @@
                 games consoles and television. Keynote talks, Q&amp;A's, breakout 
                 sessions and socialising will give you get a better understanding of the modern cross-platform web.</p>
         </div>
-        <div id="presenters">
+        <div id="presenters" class="home">
             <h2>Presenters include…</h2>
             <ul>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1150432577/speakhdr.jpg" alt="@mahemoff"> Michael Mahemoff (<a href="http://twitter.com/mahemoff/">@mahemoff</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1150432577/speakhdr.jpg" alt="@mahemoff"> Michael Mahemoff (<a href="http://twitter.com/mahemoff/">@mahemoff</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1648506030/denmark.png" alt="@thebeebs"> Martin Beeby (<a href="http://twitter.com/thebeebs/">@thebeebs</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1648506030/denmark.png" alt="@thebeebs"> Martin Beeby (<a href="http://twitter.com/thebeebs/">@thebeebs</a>)
                 </li>
 				<li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1213141308/redux-yellow.jpg" alt="@patrick_h_lauke"> Patrick Lauke (<a href="http://twitter.com/patrick_h_lauke/">@patrick_h_lauke</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1213141308/redux-yellow.jpg" alt="@patrick_h_lauke"> Patrick Lauke (<a href="http://twitter.com/patrick_h_lauke/">@patrick_h_lauke</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1666904408/codepo8.png" alt="@codepo8"> Christian Heillman (<a href="http://twitter.com/codepo8/">@codepo8</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1666904408/codepo8.png" alt="@codepo8"> Christian Heillman (<a href="http://twitter.com/codepo8/">@codepo8</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/391831398/peter_opie.png" alt="@stopsatgreen"> Peter Gasston (<a href="http://twitter.com/stopsatgreen/">@stopsatgreen</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/391831398/peter_opie.png" alt="@stopsatgreen"> Peter Gasston (<a href="http://twitter.com/stopsatgreen/">@stopsatgreen</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1280643875/me2.jpg" alt="@paul_kinlan"> Paul Kinlan (<a href="http://twitter.com/paul_kinlan/">@paul_kinlan</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1280643875/me2.jpg" alt="@paul_kinlan"> Paul Kinlan (<a href="http://twitter.com/paul_kinlan/">@paul_kinlan</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/150326469/n835305082_6420882_6480795.jpg" alt="@flexewebs"> Jason Grant (<a href="http://twitter.com/flexewebs/">@flexewebs</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/150326469/n835305082_6420882_6480795.jpg" alt="@flexewebs"> Jason Grant (<a href="http://twitter.com/flexewebs/">@flexewebs</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1232663381/5388246284_df5b90b720_b-1.jpeg" alt="@seb_ly"> Seb Lee-Delisle (<a href="http://twitter.com/seb_ly/">@seb_ly</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1232663381/5388246284_df5b90b720_b-1.jpeg" alt="@seb_ly"> Seb Lee-Delisle (<a href="http://twitter.com/seb_ly/">@seb_ly</a>)
                 </li>
                 <li>
-                    <h3><img src="http://a0.twimg.com/profile_images/1539099529/me4.jpg" alt="@richtibbet"> Rich Tibbett (<a href="http://twitter.com/richtibbett/">@richtibbett</a>)</h3>
+                    <img src="http://a0.twimg.com/profile_images/1539099529/me4.jpg" alt="@richtibbet"> Rich Tibbett (<a href="http://twitter.com/richtibbett/">@richtibbett</a>)
                 </li>
             </ul>
         </div>            

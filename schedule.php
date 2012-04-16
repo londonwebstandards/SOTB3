e6bbc5a00f9fe33ca774354454b8c5f024d2d8ce
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-16 15:36:52 +0100

    Fix typos

--- schedule.php
+++ schedule.php
@@ -14,7 +14,7 @@
 		</li>
         <li><h2><span class="time">10:15</span> No app is an island</h2>
 		<p>Paul Kinlan, Google</p>
-		<p>The web is like a continent with roads linking everywhere to everywhere.  Apps are like isolated islands in an uncharted sea.  Learn how Web Intents builds bridges between these apps and to the rest of the web.  "</p>
+		<p>The web is like a continent with roads linking everywhere to everywhere.  Apps are like isolated islands in an uncharted sea.  Learn how Web Intents builds bridges between these apps and to the rest of the web.</p>
 		</li>
         <li><h2><span class="time">11:00</span> Break</li></h2>
         <li><h2><span class="time">11:15</span> The Web As It Should Be</h2>
@@ -22,7 +22,7 @@
 
         <p>With the advent of CSS3 and the ever-greater maturity of HTML5, it’s an exciting time for the Web. The possibilities for creating truly engaging – even addictive – Web and business applications appear limitless. And, with the release of Internet Explorer 9, Microsoft finally came in from the cold after many years in the world wide wilderness.</p>
 
-        <p>As Internet Explorer moves towards full support of standards-based HTML5 and CSS3, this is a great opportunity to hear from Martin Beeby about how these key technologies will be used to build and deliver the applications of tomorrow, engage end-users like never before, and generate even greater loyalty to Web-sites."</p></li>
+        <p>As Internet Explorer moves towards full support of standards-based HTML5 and CSS3, this is a great opportunity to hear from Martin Beeby about how these key technologies will be used to build and deliver the applications of tomorrow, engage end-users like never before, and generate even greater loyalty to Web-sites.</p></li>
         <li><h2><span class="time">12:00</span> Chris Heillman, Mozilla</h2></li>
         <li><span class="time">12:45</span> Lunch</li>
 		<li><h2><span class="time">13:45</span> Battle of the Browsers</h2>

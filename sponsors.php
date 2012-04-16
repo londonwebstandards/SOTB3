d7504f638a5daf2d097895a0dc2a5cb6307ed676
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-16 15:41:12 +0100

    comment Moo out

--- sponsors.php
+++ sponsors.php
@@ -18,13 +18,18 @@
             <li>
 	<a class="logo" href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a>
 	<p>Squiz is a Supported Open Source Web Services company and our clients are our number one priority. Our team's focus is to create successful web solutions which enable our clients to excel online and meet their business objectives.</p></li>
+	<?/*
 	        <li>
 		<a class="logo" href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a>
 		<p>We're a new kind of online printing business. We help our customers print things like
 		<a href="http://uk.moo.com/products/business-cards.html">Business Cards</a>,
 		<a href="http://uk.moo.com/products/postcards.html">Postcards</a> and
 		<a href="http://uk.moo.com/products/minicards.html">MiniCards</a>, making it easy for them to share information about themselves or their business in the real world.</p>
-		</li>        
+		</li>  
+	*/?>  <li>
+		<a class="logo" href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a>
+		<p>E-NOISE is a small company based in East London (UK) specialising in hosting & building WordPress and Joomla! Websites. We pride ourselves in providing friendly human support and making hosting your website simple. We also develop custom web applications, plugins and themes.</p>
+		</li>    
         </ul>
     </div>
 
@@ -36,10 +41,7 @@
 				<p>Potato plays home to a multi-disciplined team of webdev experts, tackling projects that range from complex marketing initiatives like <a href="http://p.ota.to/work/google-engage/">Google Engage</a> and massive-scale <a href="http://p.ota.to/work/youtube/">YouTube channels</a>, to fun, interesting diversions like our <a href="http://p.ota.to/work/getfavicon/">getFavicon tool</a> (which serves over 100 req/sec).</p>
 				<p>We love working in the cloud, and as such develop almost solely using services like Google App Engine. As Potato continues to grow we’re also on the hunt for developers to <a href="http://p.ota.to/jobs/">join our team</a>.</p>
 			</li>
-	        <li>
-		<a class="logo" href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a>
-		<p>E-NOISE is a small company based in East London (UK) specialising in hosting & building WordPress and Joomla! Websites. We pride ourselves in providing friendly human support and making hosting your website simple. We also develop custom web applications, plugins and themes.</p>
-		</li>
+	        
 	        <li><a class="logo" href="http://seb.ly/training"><img src="img/creative-js.png" alt="CreativeJS Training" height="56" width="160"></a>
 		<p>Come and learn how to draw, animate, and create truly interactive content, including advanced mouse interactions, pixel manipulation, image blending, physics, 3D and games. In-browser graphical capabilities are becoming richer than ever, and you can now create beautiful visual effects with JavaScript and HTML5.</p></li>           
         </ul>

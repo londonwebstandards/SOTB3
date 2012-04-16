98d31b440459f372ee03278e9e5b38a17624f235
Jim O'Donnell <jim@hoopassociates.co.uk>
2012-04-16 10:26:17 +0100

    updated schedule

--- schedule.php
+++ schedule.php
@@ -13,22 +13,23 @@
 		<p>Paul Kinlan, Google</p>
 		<p>The web is like a continent with roads linking everywhere to everywhere.  Apps are like isolated islands in an uncharted sea.  Learn how Web Intents builds bridges between these apps and to the rest of the web.  "</p>
 		</li>
-        <li><h2><span class="time">10:55</span>	Break</li></h2>
-        <li><h2><span class="time">11:10</span>	The Web As It Should Be</h2>
-        <p>Martin Beeby(@thebeebs), Internet Explorer</p>
+        <li><h2><span class="time">11:00</span>	Break</li></h2>
+        <li><h2><span class="time">11:15</span>	The Web As It Should Be</h2>
+        <p>Martin Beeby, Microsoft</p>
 
         <p>With the advent of CSS3 and the ever-greater maturity of HTML5, it’s an exciting time for the Web. The possibilities for creating truly engaging – even addictive – Web and business applications appear limitless. And, with the release of Internet Explorer 9, Microsoft finally came in from the cold after many years in the world wide wilderness.</p>
 
         <p>As Internet Explorer moves towards full support of standards-based HTML5 and CSS3, this is a great opportunity to hear from Martin Beeby about how these key technologies will be used to build and deliver the applications of tomorrow, engage end-users like never before, and generate even greater loyalty to Web-sites."</p></li>
-        <li><h2><span class="time">11:50</span>	Chris Heillman, Mozilla</h2></li>
-        <li><h2><span class="time">12:30</span>	Lunch (a varied complementary lunch will be provided)</h2></li>
-        <li><h2><span class="time">13:30</span>	HTML5: A matter of good form</h2>
-        <p>Patrick Lauke (@patrick_h_lauke), Opera</p>
-
-        <p>TBC</p></li>
-        <li><h2><span class="time">13:25</span>	Break</h2></li>
-        <li><h2><span class="time">13:40</span> Breakout session 1</h2> </li>
-        <li><h2><span class="time">14:50</span> Breakout session 2</h2></li>
+        <li><h2><span class="time">12:00</span>	Chris Heillman, Mozilla</h2></li>
+        <li><span class="time">12:45</span>	Lunch</li>
+		<li><h2><span class="time">13:45</span>	Battle of the Browsers</h2>
+		<p>Seb Lee-DeLisle, CreativeJS</p></li>
+        <li><h2><span class="time">14:00</span>	HTML5: A matter of good form</h2>
+        <p>Patrick Lauke, Opera</p>
+		</li>
+        <li><h2><span class="time">14:45</span> Breakout session 1</h2> </li>
+		<li><span class="time">15:30</span> Break </li>
+        <li><h2><span class="time">15:45</span> Breakout session 2</h2></li>
 <?
 /*
 		<li>Anything marked with * will be repeated across both breakout sessions.	
@@ -49,8 +50,7 @@
         Chris Mills, Opera</li></ul></li>
 */
 ?>
-        <li><h2><span class="time">15:50</span>	Break</h2></li>
-        <li><h2><span class="time">16:10</span>	Panel Q&amp;A Discussion</h2></li>
+        <li><h2><span class="time">16:30</span>	Panel Q&amp;A Discussion</h2></li>
         <li><h2><span class="time">17:00</span>	Drinks at the Pilot Inn</h2></li>
 		</ul>
 

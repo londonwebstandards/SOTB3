93a39bd460b3d612ab400517a6483f65eea1263e
Jim O'Donnell <jim@eatyourgreens.org.uk>
2012-04-18 11:42:58 +0100

    Link to the eventbrite ticket page

--- inc/navigation.php
+++ inc/navigation.php
@@ -3,6 +3,7 @@
         <li><a href="presenters.php">Presenters</a></li>        
         <li><a href="schedule.php">Schedule</a></li>        
         <li><a href="sponsors.php">Sponsors</a></li>
-        <li><a href="location.php">Location</a></li>        
+        <li><a href="location.php">Location</a></li>
+        <li><a href="http://lwsbrowser2.eventbrite.co.uk/">Buy tickets</a></li>
     </ul>
 </nav>

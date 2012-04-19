f157e3fd422f0935275469865aca62b89a2b30ea
Jim O'Donnell <jim@eatyourgreens.org.uk>
2012-04-19 10:23:06 +0100

    Amended schedule. Removed Opera presentation.

--- schedule.php
+++ schedule.php
@@ -27,17 +27,14 @@
 	<p>Chris Heilmann, Mozilla</p>
 	<p>It's been quite some while that the marketing drums heralded HTML5 as "The Flash Killer" and the future of the web and apps. Time to take a look at what happens and what we can do with the set of tools we have at our disposal. Chris Heilmann will go through a series of questions asked by people not in the "HTML5 scene" and how far we are when it comes to being able to answer them. We'll try to see what the barriers are and what can be done to remove them. HTML5 and friends can be a great app platform - if we play it to its strengths and not try to emulate what others do. Chris will show what Mozilla is doing to make this happen and how you could be a part of this.</p>
 	</li>
-        <li><span class="time">12:45</span> Lunch</li>
-		<li><h2><span class="time">13:45</span> Battle of the Browsers</h2>
+		<li><h2><span class="time">12:45</span> Battle of the Browsers</h2>
 		<p>Seb Lee-DeLisle, CreativeJS</p>
 		<p>After a full day of Chrome, Firefox and Opera bigging themselves up, it's time to put their money where their mouth is. Yeah sure, they can all parse HTML and be all accessible and stuff. But now's the time for the most irrelevant test of all! In code written from scratch, Seb will find out which browser can render the most 3D pixels ever!</p>
 		</li>
-        <li><h2><span class="time">14:00</span> Opera, TBC</h2>
-		</li>
+        <li><span class="time">13:00</span> Lunch</li>
+        <li><h2><span class="time">14:00</span> Breakout session 1</h2> </li>
 		<li><span class="time">14:45</span> Break </li>
-        <li><h2><span class="time">14:50</span> Breakout session 1</h2> </li>
-		<li><span class="time">15:35</span> Break </li>
-        <li><h2><span class="time">15:50</span> Breakout session 2</h2></li>
+        <li><h2><span class="time">15:00</span> Breakout session 2</h2></li>
 <?
 /*
 		<li>Anything marked with * will be repeated across both breakout sessions.	
@@ -58,7 +55,8 @@
         Chris Mills, Opera</li></ul></li>
 */
 ?>
-        <li><h2><span class="time">16:35</span> Panel Q&amp;A Discussion</h2></li>
+        <li><span class="time">15:45</span> Break </li>
+		<li><h2><span class="time">16:00</span> Panel Q&amp;A Discussion</h2></li>
         <li><span class="time">17:00</span>  Drinks at the Pilot Inn</li>
 		</ul>
 

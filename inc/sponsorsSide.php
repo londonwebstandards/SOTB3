c8d37cdedb05e8f9da2d49d4ab1239778eed4940
Morena Fiore <info@morenafiore.com>
2012-04-21 14:10:10 +0100

    Fix to FTlabs logo url in sidebar

--- inc/sponsorsSide.php
+++ inc/sponsorsSide.php
@@ -1,7 +1,7 @@
 <div id="sponsors" class="four columns offset-by-one">
     <h2>Sponsors</h2>
     <ul>
-		<li><a class="logo" href="http://assanka.com"><img src="img/ft-labs.png" alt="FT Labs" width="160"></a></li>
+		<li><a class="logo" href="http://labs.ft.com "><img src="img/ft-labs.png" alt="FT Labs" width="160"></a></li>
         <li><a class="logo" href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
         <?//<li><a class="logo" href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>?>
         <li><a class="logo" href="http://p.ota.to/"><img src="img/potato.png" alt="Potato" height="182" width="160"></a></li>

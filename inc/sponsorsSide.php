1a6081c5d3120c35e9b73d2c5867d821fa231691
Morena Fiore <info@morenafiore.com>
2012-04-21 14:16:06 +0100

    Fix to FTlabs logo url in sidebar

--- inc/sponsorsSide.php
+++ inc/sponsorsSide.php
@@ -1,11 +1,11 @@
 <div id="sponsors" class="four columns offset-by-one">
     <h2>Sponsors</h2>
     <ul>
-		<li><a class="logo" href="http://labs.ft.com "><img src="img/ft-labs.png" alt="FT Labs" width="160"></a></li>
-        <li><a class="logo" href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
+        <li><a class="logo" href="http://labs.ft.com" target="blank"><img src="img/ft-labs.png" alt="FT Labs" width="160"></a></li>
+        <li><a class="logo" href="http://squiz.co.uk" target="blank"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a></li>
         <?//<li><a class="logo" href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a></li>?>
-        <li><a class="logo" href="http://p.ota.to/"><img src="img/potato.png" alt="Potato" height="182" width="160"></a></li>
-        <li><a class="logo" href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a></li>
-        <li><a class="logo" href="http://seb.ly/training"><img src="img/creativejs.jpg" alt="CreativeJS Training" width="160"></a></li>
+        <li><a class="logo" href="http://p.ota.to/" target="blank"><img src="img/potato.png" alt="Potato" height="182" width="160"></a></li>
+        <li><a class="logo" href="http://e-noise.com" target="blank"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a></li>
+        <li><a class="logo" href="http://seb.ly/training" target="blank"><img src="img/creativejs.jpg" alt="CreativeJS Training" width="160"></a></li>
     </ul>
 </div>

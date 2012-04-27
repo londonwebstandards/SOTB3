d84654d12402ad62d706e5efe70e2f175a5c22ca
lwsftp <lwsftp@floyd.dreamhost.com>
2012-04-26 17:50:58 -0700

    Committing everything from live site.

--- sponsors.php
+++ sponsors.php
@@ -1,50 +1,55 @@
 <?php include("inc/pageopen.php"); ?>
 <?php include("inc/navigation.php"); ?>
-    <div id="brand" class="sixteen columns">
-        <h1>Sponsors</h1>
-    </div>
-    <div id="sponsors">
+<div id="brand" class="sixteen columns">
+    <h1>Sponsors</h1>
+</div>
+<div id="sponsors">
     <div class="eight columns">
         <ul>
-			<li>
-				<a class="logo" href="http://labs.ft.com"><img src="img/ft-labs.png" alt="FT Labs" width="160"></a>
-				<p>The Financial Times is one of the world’s leading business news and information organisations, recognised for our authority, integrity and accuracy.
-				FT Labs is an emerging web technologies group within the FT,
-				responsible for producing our HTML5 web app at <a href="http://app.ft.com">app.ft.com</a>, as well as
-				other innovative new ways of bringing FT content and services to
-				readers wherever they are</p>
-			</li>
             <li>
-	<a class="logo" href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a>
-	<p>Squiz is a Supported Open Source Web Services company and our clients are our number one priority. Our team's focus is to create successful web solutions which enable our clients to excel online and meet their business objectives.</p></li>
-	<?/*
-	        <li>
-		<a class="logo" href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a>
-		<p>We're a new kind of online printing business. We help our customers print things like
-		<a href="http://uk.moo.com/products/business-cards.html">Business Cards</a>,
-		<a href="http://uk.moo.com/products/postcards.html">Postcards</a> and
-		<a href="http://uk.moo.com/products/minicards.html">MiniCards</a>, making it easy for them to share information about themselves or their business in the real world.</p>
-		</li>  
-	*/?>  <li>
-		<a class="logo" href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a>
-		<p>E-NOISE is a small company based in East London (UK) specialising in hosting & building WordPress and Joomla! Websites. We pride ourselves in providing friendly human support and making hosting your website simple. We also develop custom web applications, plugins and themes.</p>
-		</li>    
+                <a class="logo" href="http://labs.ft.com"><img src="img/ft-labs.png" alt="FT Labs" width="160"></a>
+                <p>The Financial Times is one of the world’s leading business news and information organisations, recognised for our authority, integrity and accuracy.
+                    FT Labs is an emerging web technologies group within the FT,
+                    responsible for producing our HTML5 web app at <a href="http://app.ft.com">app.ft.com</a>, as well as
+                    other innovative new ways of bringing FT content and services to
+                    readers wherever they are</p>
+            </li>
+            <li>
+                <a class="logo" href="http://squiz.co.uk"><img src="img/squiz.png" alt="Squiz" height="60" width="160"></a>
+                <p>Squiz is a Supported Open Source Web Services company and our clients are our number one priority. Our team's focus is to create successful web solutions which enable our clients to excel online and meet their business objectives.</p></li>
+            <? /*
+              <li>
+              <a class="logo" href="http://moo.com"><img src="img/moo.png" alt="Moo dot com" height="170" width="160"></a>
+              <p>We're a new kind of online printing business. We help our customers print things like
+              <a href="http://uk.moo.com/products/business-cards.html">Business Cards</a>,
+              <a href="http://uk.moo.com/products/postcards.html">Postcards</a> and
+              <a href="http://uk.moo.com/products/minicards.html">MiniCards</a>, making it easy for them to share information about themselves or their business in the real world.</p>
+              </li>
+             */ ?>  <li>
+                <a class="logo" href="http://e-noise.com"><img src="img/e-noise.png" alt="E-Noise" height="82" width="160"></a>
+                <p>E-NOISE is a small company based in East London (UK) specialising in hosting & building WordPress and Joomla! Websites. We pride ourselves in providing friendly human support and making hosting your website simple. We also develop custom web applications, plugins and themes.</p>
+            </li>
+
+            <li>
+                <a class="logo" href="http://www.annalect.com/"><img src="img/annalect.jpg" alt="Annalect Group" height="51" width="160"></a>
+                <p>Annalect creates and maintains <strong>enterprise marketing solutions</strong> for clients and agencies that deliver deep insights and accurate audience targeting to drive sales and improve ROI.  These solutions are made possible by the wave of innovation in digital, data and analytics.</p>
+            </li>
         </ul>
     </div>
 
     <div class="eight columns">
         <ul>
-			<li>
-				<a class="logo" href="http://p.ota.to/"><img src="img/potato.png" alt="Potato" height="182" width="160"></a>
-				<p>Founded in 2010, Potato is a rapidly-growing creator of complex web apps, working with companies like Google, BBH, Mother and PayPal from London, Bristol, Amsterdam and San Francisco.</p>
-				<p>Potato plays home to a multi-disciplined team of webdev experts, tackling projects that range from complex marketing initiatives like <a href="http://p.ota.to/work/google-engage/">Google Engage</a> and <a href="http://p.ota.to/work/getting-business-online/">Getting Business Online</a>, to massive-scale <a href="http://p.ota.to/work/youtube/">YouTube channels</a>.</p>
-				<p>We love working in the cloud, and as such develop almost solely using services like Google App Engine. As Potato continues to grow we’re also on the hunt for developers to <a href="http://p.ota.to/jobs/">join our team</a>.</p>
-			</li>
-	        
-	        <li><a class="logo" href="http://seb.ly/training"><img src="img/creativejs.jpg" alt="CreativeJS Training" width="160"></a>
-		<p>In this acclaimed series of sell-out workshops, Seb Lee-Delisle will show you how easy it can be to create visual effects with JavaScript and HTML5. These engaging classes look at image blending, physics, games, 3D, advanced mouse interactions, and pixel manipulation. New for 2012 - CreativeJS for non-coders, part of Seb's campaign to help designers discover the joy of code. More info and dates at <a href="http://seb.ly/training">seb.ly/training</a>.</p></li>           
+            <li>
+                <a class="logo" href="http://p.ota.to/"><img src="img/potato.png" alt="Potato" height="182" width="160"></a>
+                <p>Founded in 2010, Potato is a rapidly-growing creator of complex web apps, working with companies like Google, BBH, Mother and PayPal from London, Bristol, Amsterdam and San Francisco.</p>
+                <p>Potato plays home to a multi-disciplined team of webdev experts, tackling projects that range from complex marketing initiatives like <a href="http://p.ota.to/work/google-engage/">Google Engage</a> and <a href="http://p.ota.to/work/getting-business-online/">Getting Business Online</a>, to massive-scale <a href="http://p.ota.to/work/youtube/">YouTube channels</a>.</p>
+                <p>We love working in the cloud, and as such develop almost solely using services like Google App Engine. As Potato continues to grow we’re also on the hunt for developers to <a href="http://p.ota.to/jobs/">join our team</a>.</p>
+            </li>
+
+            <li><a class="logo" href="http://seb.ly/training"><img src="img/creativejs.jpg" alt="CreativeJS Training" width="160"></a>
+                <p>In this acclaimed series of sell-out workshops, Seb Lee-Delisle will show you how easy it can be to create visual effects with JavaScript and HTML5. These engaging classes look at image blending, physics, games, 3D, advanced mouse interactions, and pixel manipulation. New for 2012 - CreativeJS for non-coders, part of Seb's campaign to help designers discover the joy of code. More info and dates at <a href="http://seb.ly/training">seb.ly/training</a>.</p></li>           
         </ul>
     </div>
 </div>
-    <?php include("inc/communityPartners.php"); ?>
+<?php include("inc/communityPartners.php"); ?>
 <?php include("inc/footer.php"); ?>

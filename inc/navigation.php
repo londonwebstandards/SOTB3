889518097f1c4eadd163a93053c816bddc73e705
Morena Fiore <info@morenafiore.com>
2013-04-10 15:11:53 +0100

    Updated, speakers

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,7 +1,7 @@
 <nav class="clearfix">
     <ul>
         <li><a href="presenters.php">Presenters</a></li>
-        <li><a href="schedule.php">Schedule</a></li>
+        <?php /*<li><a href="schedule.php">Schedule</a></li>*/?>
         <li><a href="location.php">Location</a></li>
         <li><a href="sponsors.php">Sponsors</a></li>
         <li><a href="project.php">Show off your JS</a></li>

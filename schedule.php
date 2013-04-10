630c4d8fc5b3cb4a3c8fe98f7173043276001788
Morena Fiore <info@morenafiore.com>
2013-04-10 15:30:45 +0100

    Schedule updated

--- schedule.php
+++ schedule.php
@@ -7,56 +7,69 @@
 <div class="eleven columns">
 
     <ul id="schedule">
-        <li><span class="time">8:45</span> Registration</li>
-        <li><h2><span class="time">9:30</span> Welcome talk</h2>
-            <p>The web has evolved from a document exchange protocol into a platform for rich internet applications. But there's much more to the web than video, canvas, and a hundred other APIs. There are unique, underlying, principles for both the technology and the evolution of that technology. As the web faces growing challenges such as a proliferation of form factors, sensors, and interaction styles, will these principles help or hinder?</p>
+        <li>
+            <h2><span class="time">8:45</span> Registration [Foyer]</h2>
+        </li>
+        <li>
+            <h2><span class="time">9:30</span> Welcome talk[Main Room]</h2>
         </li>
 
-        <li><h2><span class="time">10:00</span> "WebKit Monoculture Isn't Breaking the Web: You Are"</h2>
+        <li><h2><span class="time">10:00</span> "WebKit Monoculture Isn't Breaking the Web: You Are"[Main Room]</h2>
             <p>Alp Toker</p>
             <p>Is browser monoculture really good for the future of the web? Will the uneasy alliance between stakeholders reach fever pitch in 2013? What’s coming after HTML5, and is the dream of an open web once again under threat?</p>
             <p>The WebKit Open Source project is a powerhouse of innovation on web standards and browser technology. Driven by an unlikely collaboration between tech giant rivals and a regiment of rockstar coders, WebKit is becoming the de-facto browser engine for desktop web browsers, mobiles and connected devices.</p>
             <p>In this talk, Alp Toker will lay bare the state of WebKit today and explain why we all need work together now to build a better future for the open web.</p>
         </li>
 
-        <li><span class="time">10:35</span> Break</li>
+        <li>
+            <h2><span class="time">10:35</span> Break [Foyer]</h2>
+        </li>
 
-        <li><h2><span class="time">10:50</span> Internet Explorer - Touch the web</h2>
+        <li><h2><span class="time">10:50</span> Internet Explorer - Touch the web [Main Room]</h2>
             <p>Martin Beeby, Microsoft</p>
             <p>Internet Explorer 10 is now available on Windows 7 and Windows 8, in this hour long session Martin will take a look at IE10 and explain what’s new and what's changed with Microsoft’s latest browser. From using Responsive Web design to support all of the orientations of Windows 8 to Multi Touch, Martin will explain everything he’s learnt in the first 4 months of release.</p>
         </li>
 
-        <li><h2><span class="time">11:25</span> State of the Mobile Web</h2>
+        <li><h2><span class="time">11:25</span> State of the Mobile Web [Main Room]</h2>
             <p>Paul Kinlan, Chrome</p>
             <p>Mobile. Mobile. Mobile.  Understanding the state of the mobile web and where it is going is critically important for you and your business. In this talk we will look at the current state of the mobile web and the advancements that are due to land over the next year so that you can deliver great sites now and tomorrow.</p>
         </li>
 
-        <li><h2><span class="time">12:00</span> Business as usual: bringing the world wide web to the whole wide world</h2>
+        <li><h2><span class="time">12:00</span> Business as usual: bringing the world wide web to the whole wide world [Main Room]</h2>
             <p>Andreas Bovens, Opera</p>
             <p>In mid-February, Opera announced it will transition its products from Presto to WebKit. I will talk about what this shift entails, focus on our new browser products and features, and cover what changes for developers and users.</p>
         </li>
 
-        <li><h2><span class="time">12:35</span> Making your HTML5 efforts worth-while</h2>
+        <li><h2><span class="time">12:35</span> Making your HTML5 efforts worthwhile [Main Room]</h2>
             <p>Chris Heilmann, Mozilla</p>
             <p>When the web was defined as an idea it was based on the principle of independence of hardware, global location, prosperity or ability. This changed drastically when the mobile web came around and we got sucked into a world of software dependent on certain hardware and global location. HTML5 and the mobile web based on open technologies became something that needed conversion to native code to access the new hardware people use. This is against the main principle of the web and means we duplicate efforts all over the place. In this talk Chris Heilmann shows how Mozilla is battling this trend and how brushing up your HTML5 solutions allows you to reach millions of new users forgotten by native technology but nevertheless eager to be online.</p>
         </li>
 
-        <li><span class="time">13:10</span>Lunch (Complimentary with Vegeterian and Vegan options) </li>
+        <li>
+            <h2><span class="time">13:10</span>Lunch (Complimentary with Vegeterian and Vegan options) [Foyer]</h2>
+        </li>
 
 
-        <li><h2><span class="time">14:10</span> Breakout session 1</h2>
+        <li><h2><span class="time">14:10</span> Breakout session 1 [Upstairs, Rooms TBC]</h2>
             <ul>
-                <li>Pete Gasston (<a href="http://www.twitter.com/stopsatgreen" target="_blank">@stopsatgreen</a>) - “Web Components: Getting Started"</li>
-                <li>Jake Archibald (<a href="http://www.twitter.com/stopsatgreen" target="_blank">@stopsatgreen</a>) - "HTML5 Human Interaction”</li>
-                <li>Chris Heilmann (<a href="http://www.twitter.com/stopsatgreen" target="_blank">@stopsatgreen</a>) - “The bleeding edge of web development needs blood donors”</li>
-                <li>Jason Grant (<a href="http://www.twitter.com/stopsatgreen" target="_blank">@stopsatgreen</a>) - “Designing responsively”</li>
-                <li>Bruce Lawson (<a href="http://www.twitter.com/stopsatgreen" target="_blank">@stopsatgreen</a>) - “Responsive Web Design: the specs behind the sex”</li>
+                <li><h2>Pete Gasston (<a href="http://www.twitter.com/stopsatgreen" target="_blank">@stopsatgreen</a>) - “Web Components: Getting Started"</h2></li>
+                <li><h2>Jake Archibald (<a href="http://www.twitter.com/jaffathecake" target="_blank">@jaffathecake</a>) - "Rendering without lumpy bits”</h2></li>
+                <li><h2>Laura Kalbag (<a href="http://www.twitter.com/laurakalbag" target="_blank">@laurakalbag</a>) - “Design theory for the web”</h2></li>
+                <li><h2>Priya Prakash (<a href="http://www.twitter.com/priyascape" target="_blank">@priyascape</a>) - “Can you change the world through the browser? The truth about building a social impact MVP”</h2></li>
+                <li><h2>Michael R Lorek (<a href="http://www.twitter.com/m_lorek" target="_blank">@m_lorek</a>) - “Architectural Aspects within HTML5 [Semantic]”</h2></li>
+                <li><h2>Ben MacGowan (<a href="http://www.twitter.com/benmacgowan" target="_blank">@benmacgowan</a>) - “The trends and next steps of RWD”</h2></li>
+                <li><h2>Martin Beeby (<a href="http://www.twitter.com/thebeebs" target="_blank">@thebeebs</a>) - “Windows 8 App Development using HTML5”</h2></li>
             </ul>
         </li>
 
-        <li><span class="time">14:45</span> Break </li>
+        <li>
+            <h2><span class="time">14:45</span> Break [Upstairs' Foyer]</h2>
+        </li>
 
-        <li><h2><span class="time">14:55</span> Breakout session 2</h2>
+        <li>
+            <h2><span class="time">14:55</span> Breakout session 2 [Upstairs, Rooms TBC]</h2>
+            <p>Sessions from previous slot might repeat in the this second slot</p>
+
             <?php /*<ul>
                 <li>Martin Beeby "HTML5 Human Interaction”</li>
                 <li>Chris Heilmann “The bleeding edge of web development needs blood donors”</li>
@@ -66,14 +79,16 @@
             </ul>*/?>
         </li>
 
-        <li><span class="time">15:30</span> Break </li>
+        <li>
+            <h2><span class="time">15:30</span> Break [Foyer]</h2>
+        </li>
 
-        <li><h2><span class="time">15:45</span> Battle of the Browsers</h2>
+        <li><h2><span class="time">15:45</span> Battle of the Browsers [Main Room]</h2>
             <p>Seb Lee-DeLisle, CreativeJS</p>
             <p>Last year Seb pitted the browsers against each other by making millions of particles, in an astonishing live coding demo. This time, he'll once again be putting the browsers to the test in a new crazy (and mostly arbitrary) test. So you can find out which outperforms the others to be the king of the browsers!</p>
         </li>
 
-        <li><h2><span class="time">16:15</span> Panel Q&amp;A Discussion</h2></li>
+        <li><h2><span class="time">16:15</span> Panel Q&amp;A Discussion [Main Room]</h2></li>
 
         <li><span class="time">17:00</span>  Drinks at the Pilot Inn</li>
     </ul>

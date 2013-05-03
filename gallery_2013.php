3244abddcdfbe74a2c75773b38e0f00d2b393c94
ginestra <fginestra@hotmail.com>
2013-05-03 19:20:27 +0200

    Update gallery_2013.php

--- gallery_2013.php
+++ gallery_2013.php
@@ -10,136 +10,136 @@
             <div class="sotb3photos">
                 <ul>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0004.JPG" rel="sotb3" title="Badges">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0004.jpg" rel="sotb3" title="Badges">
                             <img src="img/sotb3/thumbnails/DSC_0004.jpg" alt="State of the Browser Badges" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0005.JPG" rel="sotb3" title="T-shirt">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0005.jpg" rel="sotb3" title="T-shirt">
                             <img src="img/sotb3/thumbnails/DSC_0005.jpg" alt="State of the Browser T-shirt" />
                         </a>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0008.JPG" rel="sotb3" title="Jake Archibald and Chris Heilmann">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0008.jpg" rel="sotb3" title="Jake Archibald and Chris Heilmann">
                             <img src="img/sotb3/thumbnails/DSC_0008.jpg" alt="State of the Browser Jake Archibald and Chris Heilmann" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0022.JPG" rel="sotb3" title="Getting ready">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0022.jpg" rel="sotb3" title="Getting ready">
                             <img src="img/sotb3/thumbnails/DSC_0022.jpg" alt="State of the Browser Getting ready" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0036.JPG" rel="sotb3" title="Jim O'Donnell">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0036.jpg" rel="sotb3" title="Jim O'Donnell">
                             <img src="img/sotb3/thumbnails/DSC_0036.jpg" alt="State of the Browser Jim O'Donnell">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0047.JPG" rel="sotb3" title="WebKit Monocolture">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0047.jpg" rel="sotb3" title="WebKit Monocolture">
                             <img src="img/sotb3/thumbnails/DSC_0047.jpg" alt="State of the Browser WebKit Monocolture" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0051.JPG" rel="sotb3" title="Alp Toker">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0051.jpg" rel="sotb3" title="Alp Toker">
                             <img src="img/sotb3/thumbnails/DSC_0051.jpg" alt="State of the Browser Alp Toker" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0075.JPG" rel="sotb3" title="Martin Beeby">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0075.jpg" rel="sotb3" title="Martin Beeby">
                             <img src="img/sotb3/thumbnails/DSC_0075.jpg" alt="State of the Browser Martin Beeby" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0076.JPG" rel="sotb3" title="Aspire S7">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0076.jpg" rel="sotb3" title="Aspire S7">
                             <img src="img/sotb3/thumbnails/DSC_0076.jpg" alt="State of the Browser Aspire S7" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0077.JPG" rel="sotb3" title="Break">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0077.jpg" rel="sotb3" title="Break">
                             <img src="img/sotb3/thumbnails/DSC_0077.jpg" alt="State of the Browser Break" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0086.JPG" rel="sotb3" title="Paul Kinlan">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0086.jpg" rel="sotb3" title="Paul Kinlan">
                             <img src="img/sotb3/thumbnails/DSC_0086.jpg" alt="State of the Browser Paul Kinlan" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0094.JPG" rel="sotb3" title="Jake Archibald's Ghost">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0094.jpg" rel="sotb3" title="Jake Archibald's Ghost">
                             <img src="img/sotb3/thumbnails/DSC_0094.jpg" alt="State of the Browser Jake Archibald's Ghost" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0095.JPG" rel="sotb3" title="Andreas Bovens">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0095.jpg" rel="sotb3" title="Andreas Bovens">
                         <img src="img/sotb3/thumbnails/DSC_0095.jpg" alt="State of the Browser Andreas Bovens" />
                     </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0113.JPG" rel="sotb3" title="Code">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0113.jpg" rel="sotb3" title="Code">
                             <img src="img/sotb3/thumbnails/DSC_0113.jpg" alt="State of the Browser Code" />
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0122.JPG" rel="sotb3" title="Contacts API">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0122.jpg" rel="sotb3" title="Contacts API">
                             <img src="img/sotb3/thumbnails/DSC_0122.jpg" alt="State of the Browser Contacts API">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0126.JPG" rel="sotb3" title="Chris Heilmann">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0126.jpg" rel="sotb3" title="Chris Heilmann">
                             <img src="img/sotb3/thumbnails/DSC_0126.jpg" alt="State of the Browser Chris Heilmann">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0127.JPG" rel="sotb3" title="Andreas Bovens">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0127.jpg" rel="sotb3" title="Andreas Bovens">
                             <img src="img/sotb3/thumbnails/DSC_0127.jpg" alt="State of the Browser Andreas Bovens">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0131.JPG" rel="sotb3" title=" Please don't hurt the web">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0131.jpg" rel="sotb3" title=" Please don't hurt the web">
                             <img src="img/sotb3/thumbnails/DSC_0131.jpg" alt="State of the Browser Please don't hurt the web">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0132.JPG" rel="sotb3" title="Steve Workman's Talk">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0132.jpg" rel="sotb3" title="Steve Workman's Talk">
                             <img src="img/sotb3/thumbnails/DSC_0132.jpg" alt="State of the Browser Steve Workman's Talk">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0144.JPG" rel="sotb3" title="Jake Archibald">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0144.jpg" rel="sotb3" title="Jake Archibald">
                             <img src="img/sotb3/thumbnails/DSC_0144.jpg" alt="State of the Browser Jake Archibald">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0152.JPG" rel="sotb3" title="Break">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0152.jpg" rel="sotb3" title="Break">
                             <img src="img/sotb3/thumbnails/DSC_0152.jpg" alt="State of the Browser Break">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0159.JPG" rel="sotb3" title="Seb Lee-Delisle and Nick Smith">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0159.jpg" rel="sotb3" title="Seb Lee-Delisle and Nick Smith">
                             <img src="img/sotb3/thumbnails/DSC_0159.jpg" alt="State of the Browser Seb Lee-Delisle and Nick Smith">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0164.JPG" rel="sotb3" title="Coding the Battle of the Browser Evangelists">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0164.jpg" rel="sotb3" title="Coding the Battle of the Browser Evangelists">
                             <img src="img/sotb3/thumbnails/DSC_0164.jpg" alt="State of the Browser Coding the Battle of the Browser Evangelists">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0173.JPG" rel="sotb3" title="Seb Lee-Delisle">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0173.jpg" rel="sotb3" title="Seb Lee-Delisle">
                             <img src="img/sotb3/thumbnails/DSC_0173.jpg" alt="State of the Browser Seb Lee-Delisle">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0177.JPG" rel="sotb3" title="Panel">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0177.jpg" rel="sotb3" title="Panel">
                             <img src="img/sotb3/thumbnails/DSC_0177.jpg" alt="State of the Browser Panel">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0184.JPG" rel="sotb3" title="Panel">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0184.jpg" rel="sotb3" title="Panel">
                             <img src="img/sotb3/thumbnails/DSC_0184.jpg" alt="State of the Browser Panel">
                         </a>
                     </li>
                     <li>
-                        <a class="fancybox" href="img/sotb3/big/DSC_0204.JPG" rel="sotb3" title="Panel">
+                        <a class="fancybox" href="img/sotb3/big/DSC_0204.jpg" rel="sotb3" title="Panel">
                             <img src="img/sotb3/thumbnails/DSC_0204.jpg" alt="State of the Browser Panel">
                         </a>
                     </li>

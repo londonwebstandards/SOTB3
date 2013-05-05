713a47314895d6ea5f2e26bc472cd34f01fcf229
Ginestra <fginestra@hotmail.com>
2013-05-05 10:44:47 +0100

    Added link to gallery

--- inc/navigation.php
+++ inc/navigation.php
@@ -1,7 +1,8 @@
 <nav class="clearfix">
     <ul>
         <!-- <li><a href="http://live.nicecreation.co.uk/lws" title="Select this link to watch the conference live" target="_blank">Watch live</a></li> -->
-        <li><a href="/">Home</a></li>
+        <li><a href="../">Home</a></li>
+        <li><a href="gallery_2013.php">Gallery 2013</a></li>
         <li><a href="presenters.php">Presenters</a></li>
         <li><a href="schedule.php">Schedule</a></li>
         <li><a href="location.php">Location</a></li>
